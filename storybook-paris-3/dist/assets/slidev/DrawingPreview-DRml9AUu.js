ab31bb328b4775888b685b316662f4622ea2a92e
Julien Déramond <juderamond@gmail.com>
2024-12-09 11:38:11 +0100

    chore(storybook-paris-3): regenerate slides

---
+++ storybook-paris-3/dist/assets/slidev/DrawingPreview-DRml9AUu.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DrawingPreview.vue_vue_type_script_setup_true_lang-CINFZG4O.js";import"../modules/vue-BOoaS5mz.js";import"../index-CYtqDCZH.js";import"../modules/shiki-CqyJGmhe.js";export{o as default};
