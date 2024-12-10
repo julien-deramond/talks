f35c05e750ebcc5dc5674e12ebf00fff56440a55
Julien Déramond <juderamond@gmail.com>
2024-12-10 23:07:33 +0100

    chore(storybook-paris-3): updates after feedback from Steve

---
+++ storybook-paris-3/dist/assets/slidev/DrawingPreview-vWP2_nbP.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DrawingPreview.vue_vue_type_script_setup_true_lang-BsSlTnP2.js";import"../modules/vue-BOoaS5mz.js";import"../index-BLrNlyfa.js";import"../modules/shiki-CqyJGmhe.js";export{o as default};
